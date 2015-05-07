d1f0cabc10a07fd1c75716d2b84020e0adac5436
upadrian <upadrian@gmail.com>
2015-05-07 12:28:16 -0300

    fixes y code beatuy
    Valida cis de 6 dígitos(xxx.xxx-x)

--- check-uruguay-ci.js
+++ check-uruguay-ci.js
@@ -1,21 +1,30 @@
-﻿function get_dc_uruguay(ci) {
-	var control = [2,9,8,7,6,3,4],sum = 0;
-	ci = String(ci);
-	if(isNaN(ci) || ci.length < 6)
-		return false;
-	while(ci.length < 7)
-		ci = "0" + ci;
-	ci = ci.split("");    
-	for(k in ci)
-		sum += ci[k] * control[k];    
-	rest = sum % 10;
-	return (rest != 0)?10-rest:rest;
+﻿/**
+ * Checker for uruguayan ci
+ * 
+ * @author upadrian@gmail.com
+ *  
+ */
+function ci(){
+	this.getdc = function(ci) {
+		var control = [2,9,8,7,6,3,4],sum = 0;
+		ci = String(ci);
+		if(isNaN(ci) || ci.length < 6)
+			return false;
+		while(ci.length < 7)
+			ci = "0" + ci;
+		ci = ci.split("");    
+		for(k in ci)
+			sum += ci[k] * control[k];    
+		rest = sum % 10;
+		return (rest != 0)?10-rest:rest;
+	}
+	this.iscorrect = function(cidc) {
+		cidc = String(cidc);
+		if(isNaN(cidc) || cidc.length < 7)
+			return false;
+		ci = cidc.substr(0,cidc.length - 1);
+		dc = cidc.substr(cidc.length - 1,1);
+		return dc == this.getdc(ci);
+	}
 }
-function ci_is_correct_uruguay(cidc) {
-	cidc = String(cidc);
-	if(isNaN(cidc) || cidc.length < 7)
-		return false;
-	ci = cidc.substr(0,cidc.length - 1);
-	dc = cidc.substr(cidc.length - 1,1);
-	return dc == get_dc_uruguay(ci);
-}
+var cicheck = new ci();
