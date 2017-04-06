a3ed68e856fb7c828b96f0f7562975c0888962b4
Adrián Suárez <upadrian@gmail.com>
2017-04-06 11:52:23 -0300

    code beauty

--- check-uruguay-ci.js
+++ check-uruguay-ci.js
@@ -1,30 +1,35 @@
 ﻿/**
  * Checker for uruguayan ci
- * 
+ *
  * @author upadrian@gmail.com
- *  
+ *
  */
-function ci(){
-	this.getdc = function(ci) {
-		var control = [2,9,8,7,6,3,4],sum = 0;
-		ci = String(ci);
-		if(isNaN(ci) || ci.length < 6)
+function CiCheck() {
+	this.getDc     = function(ci) {
+		var control = [2, 9, 8, 7, 6, 3, 4], sum = 0;
+		ci          = String(ci);
+		if(isNaN(ci) || ci.length < 6) {
 			return false;
-		while(ci.length < 7)
+		}
+		while(ci.length < 7) {
 			ci = "0" + ci;
-		ci = ci.split("");    
-		for(k in ci)
-			sum += ci[k] * control[k];    
-		rest = sum % 10;
-		return (rest != 0)?10-rest:rest;
-	}
-	this.iscorrect = function(cidc) {
-		cidc = String(cidc);
-		if(isNaN(cidc) || cidc.length < 7)
+		}
+		ci = ci.split("");
+		for(var k in ci) {
+			sum += ci[k] * control[k];
+		}
+		var rest = sum % 10;
+		return (rest != 0) ? 10 - rest : rest;
+	};
+	this.isCorrect = function(ciDc) {
+		ciDc = String(ciDc);
+		if(isNaN(ciDc) || ciDc.length < 7) {
 			return false;
-		ci = cidc.substr(0,cidc.length - 1);
-		dc = cidc.substr(cidc.length - 1,1);
-		return dc == this.getdc(ci);
+		}
+		var ci = ciDc.substr(0, ciDc.length - 1),
+		    dc = ciDc.substr(ciDc.length - 1, 1);
+		return dc == this.getDc(ci);
 	}
 }
-var cicheck = new ci();
+
+
